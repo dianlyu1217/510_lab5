1f645dba696a56fdb8fe4e96a6705311d1e70589
dianlyu1217 <146486958+dianlyu1217@users.noreply.github.com>
2024-04-23 15:38:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-import requests
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+import streamlit as st
 
-URL = 'http://api.ipify.org?format=json'
+load_dotenv()
 
-res = requests.get(URL)
-#print(res.text)
+genai.configure(api_key='AIzaSyCDvOMMRQmezW541JFrf5m_l4F2a4fNaAw')
+model = genai.GenerativeModel('gemini-pro')
 
+prompt_template = """
+You are an expert at planning oversea trips.
 
+Please take the users request and plan a comprehensive trip for them.
+
+Please include the following details:
+- The destination
+- The duration of the trip
+- The activities that will be done
+- The accommodation
+
+The user's request is:
+{prompt}
+"""
+
+def generate_content(prompt):
+    response = model.generate_content(prompt)
+    return response.text
+
+st.title("💝️ AI Daily Todo ")
+
+prompt = st.text_area("Enter what you want to do")
+if st.button("Give me a list!"):
+    reply = generate_content(prompt)
+    st.write(reply)
