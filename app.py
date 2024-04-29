82d562aef22d0110a86127adcd00b1ba7cd29603
dianlyu1217 <146486958+dianlyu1217@users.noreply.github.com>
2024-04-29 15:10:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,49 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 import streamlit as st
+from PyPDF2 import PdfReader
 
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel('gemini-pro')
 
-prompt_template = """
-You are an expert at planning oversea trips.
 
-Please take the users request and plan a comprehensive trip for them.
-
-Please include the following details:
-- The destination
-- The duration of the trip
-- The activities that will be done
-- The accommodation
-
-The user's request is:
-{prompt}
-"""
-
-def generate_content(prompt):
-    response = model.generate_content(prompt)
+def ai_gen(requirement: str, prompt: str):
+    response = model.generate_content([requirement, prompt])
     return response.text
 
-st.title("💝️ AI Daily Todo ")
 
-prompt = st.text_area("Enter what you want to do")
-if st.button("Give me a list!"):
-    reply = generate_content(prompt)
-    st.write(reply)
+# Function to extract text from PDF file
+def get_text(file):
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+    return text
+
+
+# Main function
+def main():
+    st.title("Mock Interview System📑")  # Add the heading "feedback Generator"
+    if 'resume_text' not in st.session_state:
+        st.session_state.resume_text = None
+    uploaded_file = st.file_uploader("Upload your resume (PDF)", type="pdf")
+    if uploaded_file is not None:
+        st.write("Resume Uploaded Successfully!")
+        resume_text = get_text(uploaded_file)
+        st.session_state.resume_text = resume_text
+    sNum = st.number_input("Number of suggestions you need", min_value=1, max_value=20)
+    qNum = st.number_input("Number of mock questions you need", min_value=1, max_value=20)
+    if st.button("Generate Feedback"):
+        with st.spinner("Your feedback is being generated!! Hang Tight!"):
+            # Generate feedback if both resume and job description are provided
+            if st.session_state.resume_text is not None:
+                requirement = 'Generate %s suggestions and %s possible questions based on my resume' % (sNum, qNum)
+                feedback = ai_gen(requirement, st.session_state.resume_text)
+                st.write("Generated Feedback:")
+                st.write(feedback)
+
+if __name__ == "__main__":
+    main()
